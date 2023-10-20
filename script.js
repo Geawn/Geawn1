b3bbcd9ffe9ac2572188464b971d2a813c955fb9
Geawn <136723235+Geawn@users.noreply.github.com>
2023-10-20 15:45:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function displayNumber() {
-    var inputNumber = document.getElementById("numberInput").value;
-    var number = parseFloat(inputNumber);
-    document.getElementById("output").textContent = "Tuổi của bạn là: " + number;
-  }
-  
-  var submitButton = document.getElementById("submitButton");
-  submitButton.addEventListener("click", displayNumber);
-  
+
